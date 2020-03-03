6a2dee0b0c33a73020a15186cb1282e001972c92
brharr <brian_c_harrison@hotmail.com>
2020-03-03 15:01:13 -0500

    Updates to Install

--- install/install-solution.ps1
+++ install/install-solution.ps1
@@ -3,16 +3,16 @@
 
 $location = "eastus2"
 $resourceGroup = "auto-orphan"
-$autoAccountName = "<Name of Automation Account>"
+$autoAccountName = "auto-account-orphans"
 $storageName = "azureorphanfuncstor"
 $functionAppName = "AzureOrphanFunctionApp"
 $functionAppPlanName = "AppPlan-OrphanFunctions"
 $functionOrphanURL = "https://" + $functionAppName + ".azurewebsites.net/api/AzureOrphanHttpTrigger"
 
-$azureDeployHash = @{ location = $location; functionAppServicePlan = $functionAppPlanName; functionAppName = $functionAppName }
+New-AzResourceGroup -Name $resourceGroup -Location $location
+$azureDeployHash = @{ location = $location; appName = $functionAppName; runtime = "node" }
 # Create the Azure Functions and corresponding App Plan
-New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroup `
-    -TemplateFile azuredeploy.json -TemplateParameterObject $azureDeployHash
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile azuredeploy.json -TemplateParameterObject $azureDeployHash
 
 # Someone will need to create a new Azure Application object in AAD and make sure that it is certificate based
 # Once this Application is created, make sure to get the below information and paste them into their correct
@@ -25,19 +25,19 @@ $tenantId = "feb0106c-50ef-4b39-8714-da9589109fcf"
 $subId = "3a1ad284-92e6-4b05-b32c-e96edf011ed4"
 
 # Create all of the required Automation Objects and Shared Resources
-New-AzureRmAutomationAccount -ResourceGroupName $resourceGroup -Name $autoAccountName -Location $location
+New-AzAutomationAccount -ResourceGroupName $resourceGroup -Name $autoAccountName -Location $location
 # Create the shared variable used to call the Azure Function for processing the JSON object
-New-AzureRmAutomationVariable -ResourceGroupName $resourceGroup -AutomationAccountName $autoAccountName `
+New-AzAutomationVariable -ResourceGroupName $resourceGroup -AutomationAccountName $autoAccountName -Encrypted $False `
     -Name OrphanFunctionURL -Value $functionOrphanURL `
     -Description "URL of the Azure Function that will populate the Storage Queue with Orphan JSON Data"
 # Create the Azure RunAs Connection for actually processing Azure SDK calls
 $fieldValues = @{ ApplicationId = $spnAppId; TenantId = $tenantId; SubscriptionId = $subsId; CertificateThumbprint = $spnCertThumb }
-New-AzureRmAutomationConnection -ResourceGroupName $resourceGroup -AutomationAccountName -$autoAccountName -Name AzureRunAsConnection -ConnectionTypeName AzureServicePrincipal -ConnectionFieldValues $fieldValues
+New-AzAutomationConnection -ResourceGroupName $resourceGroup -AutomationAccountName $autoAccountName -Name AzureRunAsConnection -ConnectionTypeName AzureServicePrincipal -ConnectionFieldValues $fieldValues
 # Import each of the PowerShell runbooks based on the current folder structure
+Import-AzAutomationRunbook -ResourceGroupName $resourceGroup -AutomationAccountName $autoAccountName -Path "./ps-runbooks/audit-orphan-disks.ps1" -Type PowerShell
+Import-AzAutomationRunbook -ResourceGroupName $resourceGroup -AutomationAccountName $autoAccountName -Path "../ps-runbooks/audit-orphan-publicips.ps1" -Type PowerShell
 
-
-$storageAcct = New-AzStorageAccount -ResourceGroupName $resourceGroup -Location $location `
-    -Name $storageName -AccessTier Hot -Kind StorageV2 -SkuName Standard_LRS
+$storageAcct = New-AzStorageAccount -ResourceGroupName $resourceGroup -Location $location -Name $storageName -AccessTier Hot -Kind StorageV2 -SkuName Standard_LRS
 $ctx = $storageAcct.Context
 New-AzStorageQueue -Name orphans -Context $ctx
 New-AzStorageQueue -Name auditreports -Context $ctx
